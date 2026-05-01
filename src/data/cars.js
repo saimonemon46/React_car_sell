184fef3c4c49dd83268b802a307fe764e7206eb0
Md. Saimon Hasan Emon <saimonemon46@gmail.com>
2026-05-01 22:55:30 +0600

    Dummmy data

---
+++ src/data/cars.js
@@ -0,0 +1,18 @@
+const cars = [
+  {
+    id: 1,
+    name: "Tesla Model S",
+    price: 80000,
+    image: "https://via.placeholder.com/300",
+    description: "Electric luxury sedan",
+  },
+  {
+    id: 2,
+    name: "BMW M4",
+    price: 70000,
+    image: "https://via.placeholder.com/300",
+    description: "Performance sports car",
+  },
+];
+
+export default cars;
