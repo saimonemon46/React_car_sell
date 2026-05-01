467ea5783290d59e57f74f2b1c7e827de29a9d7c
Md. Saimon Hasan Emon <saimonemon46@gmail.com>
2026-05-01 23:04:39 +0600

    Dummmy

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white text-center p-4 mt-8">
+      <p>&copy; 2026 CarSell. All rights reserved.</p>
+    </footer>
+  );
+}
